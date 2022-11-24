2950d6106fcb9126b1515349956886c0d698738c
DRAC-09 <allan.estrada9@gmail.com>
2022-11-24 00:59:37 -0600

    Añadido Menu y Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { DocumentacionComponent } from './components/documentacion/documentacion.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+
+
+// Rutas de Nevegacion
+const routes: Routes = [
+  {path: ' ', redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home', component: HomeComponent },
+  {path: 'documentacion', component: DocumentacionComponent },
+  {path: 'acercade', component: AcercadeComponent },
+  {path: '**', component: PagenotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { DocumentacionComponent } from './components/documentacion/documentacion.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DocumentacionComponent,
+    AcercadeComponent,
+    MenuComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
