e951469e6ed1310c01fcc3ea3dd5a4b08507a0d9
DRAC-09 <allan.estrada9@gmail.com>
2022-11-24 23:55:24 -0600

    Actualizado componente fujoefectivo, añadido formulario reactivo

--- src/app/components/simulador/flujoefectivo/flujoefectivo.component.ts
+++ src/app/components/simulador/flujoefectivo/flujoefectivo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';                                       // Fromularios Reactivos
 
 @Component({
   selector: 'app-flujoefectivo',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlujoefectivoComponent implements OnInit {
 
+  datos = new FormGroup({
+    inversion: new FormControl(''),
+    ingresos: new FormControl(''),
+    costoProduccion: new FormControl(''),
+    anios: new FormControl(''),
+    valorSalvamento: new FormControl(''),
+    impuestos: new FormControl(''),
+    TMAR: new FormControl('')
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  calcular(){
+    console.log(this.datos.value)
+  }
+
+  limpiar(){
+    this.datos.reset()
   }
 
 }
