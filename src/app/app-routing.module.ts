98288776128da15fb13ed59475ae0c1bc47b73ff
DRAC-09 <allan.estrada9@gmail.com>
2022-11-24 19:43:50 -0600

    Añadidos componentes de Simulacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// Componentes principales
 import { HomeComponent } from './components/home/home.component';
 import { DocumentacionComponent } from './components/documentacion/documentacion.component';
 import { AcercadeComponent } from './components/acercade/acercade.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+
+// Componentes Simulador
+import { FlujoefectivoComponent } from './components/simulador/flujoefectivo/flujoefectivo.component'; 
+import { CostobeneficioComponent } from './components/simulador/costobeneficio/costobeneficio.component';
+import { PeriodorecuperacionComponent } from './components/simulador/periodorecuperacion/periodorecuperacion.component';
+import { PagoprestamoComponent } from './components/simulador/pagoprestamo/pagoprestamo.component';
 
 
 // Rutas de Nevegacion
@@ -14,6 +21,12 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent },
   {path: 'documentacion', component: DocumentacionComponent },
   {path: 'acercade', component: AcercadeComponent },
+
+  {path: 'flujoe-fectivo', component: FlujoefectivoComponent },
+  {path: 'costo-beneficio', component: CostobeneficioComponent },
+  {path: 'periodo-recuperacion', component: PeriodorecuperacionComponent },
+  {path: 'pago-prestamo', component: PagoprestamoComponent },
+  
   {path: '**', component: PagenotfoundComponent },
 ];
 
---
+++ src/app/components/simulador/flujoefectivo/flujoefectivo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flujoefectivo',
+  templateUrl: './flujoefectivo.component.html',
+  styleUrls: ['./flujoefectivo.component.css']
+})
+export class FlujoefectivoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
