559dbd3e428f6265fcc2628c9f46a373a64eff25
DRAC-09 <allan.estrada9@gmail.com>
2022-12-03 03:58:47 -0600

    Modificaciones en el tabulado del codigo

--- src/app/components/simulador/flujoefectivo/flujoefectivo.component.ts
+++ src/app/components/simulador/flujoefectivo/flujoefectivo.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';                                    
+import { Router } from '@angular/router';
                                        // Fromularios Reactivos
 
 @Component({
@@ -24,13 +24,13 @@ export class FlujoefectivoComponent implements OnInit {
 
     // depreciacion: new FormControl(this.dep)
   });
-  
+
 
   constructor(
     private router: Router,
     ) {
 
-      if (localStorage.getItem('form-fne') !== null){                                 
+      if (localStorage.getItem('form-fne') !== null){
         let form = JSON.parse(String(localStorage.getItem('form-fne')));
         this.formularioDatos.get('inversion')?.setValue(form.inversion)
         this.formularioDatos.get('ingresos')?.setValue(form.ingresos)
@@ -41,9 +41,9 @@ export class FlujoefectivoComponent implements OnInit {
         this.formularioDatos.get('tmar')?.setValue(form.tmar)
         this.siguiente = true
       }
-  
+
       if (localStorage.getItem('form-fne') == null){
-        console.log("llenar con local")
+        // console.log("llenar con local")
         this.formularioDatos.get('inversion')?.setValue(12000000)
         this.formularioDatos.get('ingresos')?.setValue(24000000)
         this.formularioDatos.get('costo')?.setValue(18000000)
@@ -88,22 +88,22 @@ export class FlujoefectivoComponent implements OnInit {
       }
 
       // Obtenemos los sifuientes datos del formulario
-      let F_INGRESOS = this.formularioDatos.get('ingresos')?.value                          // Ingresos                          
+      let F_INGRESOS = this.formularioDatos.get('ingresos')?.value                          // Ingresos
       let F_COSTOS = this.formularioDatos.get('costo')?.value                               // Costos
       let F_ISV = this.formularioDatos.get('impuestos')?.value                              // Impuesto
       let F_VS = this.formularioDatos.get('valorsalvamento')?.value                         // Valor de salvamento
-      
+
       // Con los datos obtenidos calculamos
-      let f_uai = (F_INGRESOS - F_COSTOS - this.depreciacion)                               // Utilidad antes del Impuesto         
+      let f_uai = (F_INGRESOS - F_COSTOS - this.depreciacion)                               // Utilidad antes del Impuesto
       let f_isv = f_uai*(F_ISV/100)                                                         // Impuesto
       let f_udi = (f_uai - f_isv)                                                           // Utilidad despues del impuesto
-      let f_fne = f_udi + this.depreciacion                                                 // Flujo neto de efectivo    
-      
+      let f_fne = f_udi + this.depreciacion                                                 // Flujo neto de efectivo
+
 
       let a = this.formularioDatos.get('anios')?.value                                      // Obtenemos los años del formulario
 
       // Ingresamos los datos al arreglo "flujo"
-      for (let i = 1; i <= a; i++) {                                                      
+      for (let i = 1; i <= a; i++) {
         this.flujo[i]={
           "anio":i,
           "ingresos": F_INGRESOS,
