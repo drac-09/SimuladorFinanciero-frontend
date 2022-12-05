9f018e9ae0f4fb6dfa1ac84a7ed42d69330f1447
DRAC-09 <allan.estrada9@gmail.com>
2022-12-04 23:41:38 -0600

    Actualizaciones

--- src/app/components/simulador/flujoefectivo/flujoefectivo.component.ts
+++ src/app/components/simulador/flujoefectivo/flujoefectivo.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-                                       // Fromularios Reactivos
 
 @Component({
   selector: 'app-flujoefectivo',
